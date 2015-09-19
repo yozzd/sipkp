c71080f46a3991e41e2ee04e90671bbcd2222315
yozzd <yozzdharma@gmail.com>
2015-09-19 09:23:36 +0700

    update #2

--- client/app/admin/beranda/beranda.controller.js
+++ client/app/admin/beranda/beranda.controller.js
@@ -2,5 +2,39 @@
 
 angular.module('sipkpApp')
     .controller('AdminBerandaCtrl', function ($scope) {
-        $scope.message = 'Hello';
+
+        $scope.menus = [{
+            label: 'Admin',
+            icon: 'ion ion-ios-home ion-48',
+            state: 'admin'
+        }, {
+            label: 'Create User',
+            icon: 'ion ion-person-add ion-48',
+            state: 'admin.create'
+        }, {
+            label: 'Daftar User',
+            icon: 'ion ion-clipboard ion-48',
+            state: 'admin.daftar'
+        }, {
+            label: 'Laporan Bulanan Per Kabupaten / Kota',
+            icon: 'ion ion-clipboard ion-48',
+            state: 'admin.prbt'
+        }, {
+            label: 'Laporan Bulanan Per Komoditas',
+            icon: 'ion ion-clipboard ion-48',
+            state: 'admin.prbk'
+        }, {
+            label: 'Laporan Evaluasi Bulanan',
+            icon: 'ion ion-clipboard ion-48',
+            state: 'admin.evaluasi'
+        }, {
+            label: 'SMS Sender',
+            icon: 'ion ion-android-textsms ion-48',
+            state: 'admin.sender'
+        }, {
+            label: 'Ubah Password',
+            icon: 'ion ion-lock-combination ion-48',
+            state: 'settings'
+        }];
+
     });
--- client/app/user/beranda/beranda.controller.js
+++ client/app/user/beranda/beranda.controller.js
@@ -1,6 +1,42 @@
 'use strict';
 
 angular.module('sipkpApp')
-    .controller('UserBerandaCtrl', function ($scope) {
-        $scope.message = 'Hello';
+    .controller('UserBerandaCtrl', function ($scope, Auth) {
+
+        $scope.getCurrentUser = Auth.getCurrentUser;
+
+        $scope.menus = [{
+            label: $scope.getCurrentUser().name,
+            icon: 'ion ion-ios-home ion-48',
+            state: 'user'
+        }, {
+            label: 'Input Harga Harian Produsen',
+            icon: 'ion ion-android-create ion-48',
+            state: 'user.prcreate'
+        }, {
+            label: 'Input Harga Harian Grosir',
+            icon: 'ion ion-android-create ion-48',
+            state: 'user.grcreate'
+        }, {
+            label: 'Input Harga Harian Eceran',
+            icon: 'ion ion-android-create ion-48',
+            state: 'user.eccreate'
+        }, {
+            label: 'Laporan Harian',
+            icon: 'ion ion-clipboard ion-48',
+            state: 'user.prh'
+        }, {
+            label: 'Laporan Mingguan',
+            icon: 'ion ion-clipboard ion-48',
+            state: 'user.prm'
+        }, {
+            label: 'Laporan Bulanan',
+            icon: 'ion ion-clipboard ion-48',
+            state: 'user.prb'
+        }, {
+            label: 'Ubah Password',
+            icon: 'ion ion-lock-combination ion-48',
+            state: 'settings'
+        }];
+
     });
